2fea301002401fe8764b0613729dffa5cefa3917
GuruCharan27 <727821tuad017@skct.edu.in>
2023-03-22 19:37:47 +0530

    Spring boot project added

---
+++ HostelAttendance/src/main/java/com/example/main/model/StudentUsers.java
@@ -0,0 +1,55 @@
+package com.example.main.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+
+@Transactional
+@Entity
+@Table(name = "student_users")
+public class StudentUsers {
+	
+	@Id
+	private String rollno;
+	private String name;
+	private String email;
+	private String password;
+	public String getRollno() {
+		return rollno;
+	}
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public StudentUsers(String rollno, String name, String email, String password) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+	public StudentUsers() {
+		super();
+	}
+	
+	
+
+}
