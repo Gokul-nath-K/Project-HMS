0f2a076a2e13992f03c79ef9663c68096185cfc1
Gokul-nath-K <gokulnathk003@gmail.com>
2023-03-23 09:21:51 +0530

    App js updated

--- hms-student/src/index.js
+++ hms-student/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './theme/css/theme.css';
+import './theme/css/ico/nucleo-icons.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
