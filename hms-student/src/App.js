c038996b7c49fd2dbca81731db5abd3d91d7e512
Gokul-nath-K <gokulnathk003@gmail.com>
2023-03-23 19:35:36 +0530

    Updated App.js

--- hms-student/src/App.js
+++ hms-student/src/App.js
@@ -1,30 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Login from './views/login';
 import Dashboard from './views/dashboard'
 import Profile from './views/profile'
 import Outpass from './views/outpass'
-import { userContext } from './Data/userContext'
 import Complain from './views/complain';
 
 function App() {
 
-  const [user, setUser] = useState({});
-
   return (
     <>
       <div className="App">
-        <userContext.Provider value={{ user, setUser }}>
-          <Routes>
-            <Route exact path='/' element={<Login />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/outpass' element={<Outpass />} />
-            <Route path='/complain' element={<Complain />} />
-          </Routes>
-        </userContext.Provider>
-
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/outpass' element={<Outpass />} />
+          <Route path='/complain' element={<Complain />} />
+        </Routes>
       </div>
     </>
   );
