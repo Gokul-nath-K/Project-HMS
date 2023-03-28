cff171f184ee2a39052074dcedb123e2ad52fef7
Gokul-nath-K <gokulnathk003@gmail.com>
2023-03-29 01:06:05 +0530

    Update ComplaintRepo.java

--- HostelAttendance/src/main/java/com/example/main/repository/ComplaintRepo.java
+++ HostelAttendance/src/main/java/com/example/main/repository/ComplaintRepo.java
@@ -1,9 +1,23 @@
 package com.example.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.main.model.Complaint;
 
+import jakarta.transaction.Transactional;
+
 public interface ComplaintRepo extends JpaRepository<Complaint, Integer>{
+
+    @Query("select count(c) from Complaint c where c.block = ?1 and c.status = 'pending'")
+	public List<Integer> complaintsCount(String block);
+
+	@Transactional
+	@Modifying
+	@Query("UPDATE Complaint c SET c.status = 'seen' WHERE c.rollno = ?1")
+	public void complaintapproval(String rollno);
 
 }
