0ce2ff2b5d32678106b19d890869f7c53dcd6d81
Gokul-nath-K <gokulnathk003@gmail.com>
2023-03-28 11:15:29 +0530

    Create no_announcement component

---
+++ hms-student/src/components/no_announcement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NoAnnouncement = () => {
+  return (
+    <>
+      <div className="col p-7">
+            No announcement made !
+      </div>
+    </>
+  );
+};
