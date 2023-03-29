f0c0a6f661a3c664452f8c3678588737762fc414
GuruCharan27 <727821tuad017@skct.edu.in>
2023-03-29 09:03:05 +0530

    Updated SOS Repo

--- HostelAttendance/src/main/java/com/example/main/repository/SOSRepo.java
+++ HostelAttendance/src/main/java/com/example/main/repository/SOSRepo.java
@@ -17,7 +17,7 @@ public interface SOSRepo extends JpaRepository<SOSTable, Integer> {
 	@Query("UPDATE SOSTable s SET s.isactive = true WHERE s.rollno = ?1")
 	public void sosapproval(String rollno);
 
-	@Query("select count(s) from SOSTable s where s.block = ?1 and c.isactive = false")
+	@Query("select count(s) from SOSTable s where s.block = ?1 and s.isactive = false")
 	public List<Integer> sosCount(String block);
 
 }
